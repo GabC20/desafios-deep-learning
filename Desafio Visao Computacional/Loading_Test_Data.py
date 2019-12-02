165c9ac61e776b5a7f5b5b6de99f73bbf1517623
GabC20 <gabrielsc1618@gmail.com>
2019-12-02 11:33:15 -0500

    Carregando os dados de teste e importando as bibliotecas necessárias

---
+++ Desafio Visao Computacional/Loading_Test_Data.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 11:27:11 2019
+
+@author: Gabriel Carvalho
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+import random
+import pickle
+
+
+# Definindo diretórios que contém as imagens e as classes para base de dados de testes
+
+TRAIN_DATADIR = "D:\Coisas\Desafio CyberLabs\Desafio Visao Computacional\Test_Data"
+CATEGORIES = ["AZUL", "GOL"]
+IMG_SIZE = 64
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
