43e6e4c30b5eaa84d656a69df7f7e5a086cf5603
GabC20 <gabrielsc1618@gmail.com>
2019-12-02 10:02:20 -0500

    Carregando os dados de treino e importando as bibliotecas necessárias

---
+++ Desafio Visao Computacional/Loading_Train_Data.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 09:57:28 2019
+
+@author: Gabriel Carvalho
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+import random
+import pickle
