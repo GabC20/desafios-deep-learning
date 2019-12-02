c34ae1a563f70b58804348613d6083058db4a01e
GabC20 <gabrielsc1618@gmail.com>
2019-12-02 11:36:16 -0500

    Testando imagens do conjunto de teste

--- Desafio Visao Computacional/Loading_Test_Data.py
+++ Desafio Visao Computacional/Loading_Test_Data.py
@@ -15,10 +15,21 @@ import pickle
 
 # Definindo diretórios que contém as imagens e as classes para base de dados de testes
 
-TRAIN_DATADIR = "D:\Coisas\Desafio CyberLabs\Desafio Visao Computacional\Test_Data"
+TEST_DATADIR = "D:\Coisas\Desafio CyberLabs\Desafio Visao Computacional\Test_Data"
 CATEGORIES = ["AZUL", "GOL"]
 IMG_SIZE = 64
 
+
+# Testando imagens do conjunto de teste
+
+for category in CATEGORIES:
+    path = os.path.join(TEST_DATADIR, category)
+    for img in os.listdir(path):
+        img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_COLOR)
+        plt.imshow(img_array, cmap="gray")
+        plt.show()
+        break
+    break
 
 
 
